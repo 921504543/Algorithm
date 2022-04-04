c5d6f268b813923a5b690ca4dc47c83b6d69d46d
postgraduate <921504543@qq.com>
2022-04-04 22:43:23 +0800

    BubbleSort

---
+++ src/DTcourseDataStructure/p20BubbleSort.java
@@ -0,0 +1,23 @@
+package DTcourseDataStructure;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+
+public class p20BubbleSort {
+    public static void main(String[] args) {
+        int[] arr=new int[]{9,8,7,6,5,4,3,2,1};
+        int temp=0;
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length-1-i;j++){
+                if (arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
--- src/LeetCode/T367isPerfectSquare.java
+++ src/LeetCode/T367isPerfectSquare.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 public class T367isPerfectSquare {
     public boolean isPerfectSquare(int num) {
-Math.sqrt();
+        int n=(int)Math.sqrt(num);
+        return n*n==num;
     }
 }
